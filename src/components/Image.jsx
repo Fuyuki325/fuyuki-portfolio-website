23157c2aa500bf9e95b1cb3f162d630920ce4275
Fuyuki Malahom <fuyuki325@gmail.com>
2023-12-27 01:32:28 -0800

    Aligned items in red div

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,8 +1,9 @@
 const Image = ({ imgURL }) => {
   
   return (
-    <div className="relative w-1/5 h-1/5 shadow-[-20px_16px_0px_0px] shadow-red-500">
-      <img src={imgURL} alt="Image" className="shadow-[20px_-16px_0px_0px] shadow-green-500 w-full" />
+    <div className="relative shadow-[-20px_16px_0px_0px] shadow-red-500 w-1/2 h-full lg:w-full">
+      <img src={imgURL} alt="Image" className="shadow-[20px_-16px_0px_0px] shadow-green-500 " />
+
     </div>
   );
 };
